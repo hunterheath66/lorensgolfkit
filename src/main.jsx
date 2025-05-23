76fe7107642a855295d1090b5f1c3cb11ad9fa5d
hunterheath66 <loren@llhrealestate.com>
2025-05-22 22:17:31 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GolfCardKit from './GolfCardKit'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GolfCardKit />
-  </React.StrictMode>,
-)
