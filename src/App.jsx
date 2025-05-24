7b6be92d4e6fb1a3dffe4583822f1ad4f86c1dee
hunterheath66 <loren@llhrealestate.com>
2025-05-24 18:38:13 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
 import { WarmupCard } from "./WarmupCard";
-import { MentalGameCard } from "./MentalGameCard";
 import { TailwindCard } from "./TailwindCard";
 import { HeadwindCard } from "./HeadwindCard";
 import { PracticePlanCard } from "./PracticePlanCard";
 import { DecadeCard } from "./DecadeCard";
-import { FullSwingCard } from "./FullSwingCard";
-import { ChippingPitchingCard } from "./ChippingPitchingCard";
-import { PuttingCard } from "./PuttingCard";
-import { ShortGameChallengeCard } from "./ShortGameChallengeCard";
+import { FullSwingLessonsCard } from "./FullSwingLessonsCard";
+import { ChippingPitchingLessonsCard } from "./ChippingPitchingLessonsCard";
+import { PuttingLessonsCard } from "./PuttingLessonsCard";
+import { ShortGameSituationsCard } from "./ShortGameSituationsCard";
+import { MentalGameCard } from "./MentalGameCard";
+import { CourseManagementCard } from "./CourseManagementCard";
 
 const cards = [
   { name: "Warmup", component: <WarmupCard /> },
-  { name: "Mental Game", component: <MentalGameCard /> },
   { name: "Tailwind", component: <TailwindCard /> },
   { name: "Headwind", component: <HeadwindCard /> },
   { name: "Practice Plan", component: <PracticePlanCard /> },
   { name: "DECADE", component: <DecadeCard /> },
-  { name: "Full Swing", component: <FullSwingCard /> },
-  { name: "Chipping & Pitching", component: <ChippingPitchingCard /> },
-  { name: "Putting", component: <PuttingCard /> },
-  { name: "Short Game Challenge", component: <ShortGameChallengeCard /> },
+  { name: "Full Swing", component: <FullSwingLessonsCard /> },
+  { name: "Chipping & Pitching", component: <ChippingPitchingLessonsCard /> },
+  { name: "Putting", component: <PuttingLessonsCard /> },
+  { name: "Short Game Situations", component: <ShortGameSituationsCard /> },
+  { name: "Mental Game", component: <MentalGameCard /> },
+  { name: "Course Management", component: <CourseManagementCard /> },
 ];
 
 export default function App() {
   const [index, setIndex] = useState(0);
-
   const nextCard = () => setIndex((index + 1) % cards.length);
   const prevCard = () => setIndex((index - 1 + cards.length) % cards.length);
 
@@ -40,3 +41,4 @@ export default function App() {
     </div>
   );
 }
+
